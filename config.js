922d04c06d18bccca625e383991299fc03282c0e
Kharlewu <97666423+KharleWu@users.noreply.github.com>
2024-10-31 16:34:49 -0400

    Update config.js

--- config.js
+++ config.js
@@ -16,7 +16,7 @@ var config = {
     footer: 'Source: source citations, etc. <br> Created using <a href="https://github.com/mapbox/storytelling" target="_blank">Mapbox Storytelling</a> template.',
     chapters: [
         {
-            id: 'slug-style-id',
+            id: 'name1',
             alignment: 'left',
             hidden: false,
             title: 'Hi, it is 10/31/2024',
